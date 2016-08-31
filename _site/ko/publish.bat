346b3c8153681dae82939a7f52a7a282e55b4a42
kk.kim <kk.kim@pitple.com>
2016-09-01 00:14:00 +0800

    add multi language plugin

---
+++ _site/ko/publish.bat
@@ -0,0 +1,7 @@
+git checkout master
+git branch -D gh-pages
+git checkout -b gh-pages
+git filter-branch --subdirectory-filter _site/ -f
+git push origin +gh-pages
+git push --all
+git checkout master
